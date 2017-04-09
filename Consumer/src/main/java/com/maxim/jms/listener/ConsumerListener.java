42195b40188b50bae8c695b455ec29d868518778
joesnipes <joesnipes@gmail.com>
2017-04-09 10:46:09 -0400

    initial commit

---
+++ Consumer/src/main/java/com/maxim/jms/listener/ConsumerListener.java
@@ -0,0 +1,16 @@
+package com.maxim.jms.listener;
+
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConsumerListener implements MessageListener {
+
+	@Override
+	public void onMessage(Message arg0) {
+		System.out.println("In onMessage()");
+	}
+
+}
